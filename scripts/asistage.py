ad3efd54cce5a649495dfbf2b9c79a9fe81c3d1a
lukasvasadi <lukas.vasadi@gmail.com>
2022-04-14 12:00:10 -0400

    create new function for x-axis scan with encoder output

--- scripts/asistage.py
+++ scripts/asistage.py
@@ -151,11 +151,19 @@ class MS2000(SerialPort):
         self.send_command(f"S {axis}={speed}")
         self.read_response()
     
-    def scan_x_axis(self, start: int, stop: int, enc_divide: int, num_pix: int):
-        # set ttl to output at x constant move
-        # self.ttl(y=3)
+    def scan_x_axis_enc(self, start: int, num_pix: int, enc_divide: int = 35):
         self.ttl(x=1)
         self.send_command(f"NR X={start} Z={enc_divide} F={num_pix}")
+        self.read_response()
+        self.send_command("SN X=1 Y=0 Z=0 F=0")
+        self.read_response()
+        self.send_command("SN")
+        self.read_response()
+    
+    def scan_x_axis(self, start: int, stop: int):
+        # set ttl to output at x constant move
+        self.ttl(y=3)
+        self.send_command(f"NR X={start} Y={stop}")
         self.read_response()
         self.send_command("SN X=1 Y=0 Z=0 F=0")
         self.read_response()
