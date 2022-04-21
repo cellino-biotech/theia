fa772e6fd8eb5c4947f9ac67b145bee102055771
lukasvasadi <lukas.vasadi@gmail.com>
2022-04-21 17:28:15 -0400

    change description text file to json format and correct chopping algorithm

--- scripts/scan_continuous_encoder_ttl.py
+++ scripts/scan_continuous_encoder_ttl.py
@@ -4,6 +4,7 @@
 
 import os
 import math
+import json
 import tifffile
 import numpy as np
 
@@ -125,7 +126,6 @@ def record_images(camera: object, stage: object, path: str, dirname: str, num_zo
 
         counter = 0
 
-        # while counter < total_row_acq:
         while camera.IsGrabbing():
             # use timeout of 2000ms (must be greater than exposure time)
             grab_result = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
@@ -144,32 +144,44 @@ def record_images(camera: object, stage: object, path: str, dirname: str, num_zo
     except genicam.GenericException as e:
         print(e)
     finally:
-        print(f"Total images requested: {total_row_acq}")
-        print(f"Total images recorded: {counter}")
+        print(f"total images requested: {total_row_acq}")
+        print(f"total images recorded: {counter}")
         camera.StopGrabbing()
 
         print(f'total encoder tick count: {count_task.read()}')
         count_task.close()
+
+        layer_adjustments = {}
         
-        overlap_stack = []
+        overlap = []
 
         for i in range(num_zones):
             camera.ROIZoneSelector.SetValue(f"Zone{i}")
             offset = camera.ROIZoneOffset.GetValue()
 
-            start = 1544-offset
-            stop = total_row_acq-(1544+offset)
+            start = SENSOR_HEIGHT_PIX - offset
+            stop = total_row_acq - (SENSOR_HEIGHT_PIX - start)
 
-            overlap_stack.append(reconstruction[i, start:stop, :])
+            overlap.append(reconstruction[i, start:stop, :])
 
-        # zstack = np.stack(overlap_stack, axis=0)
-        zstack = np.stack(overlap_stack, axis=0)
-        save_image_array(zstack, os.path.join(path, dirname + "_stack.tif"))
+            layer_adjustments[f"{i}"] = {
+                "offset": offset, 
+                "start": start,
+                "stop": stop
+            }
+        
+        save_image_array(np.stack(reconstruction, axis=0), os.path.join(path, dirname + "_raw.tif"))
+        save_image_array(np.stack(overlap, axis=0), os.path.join(path, dirname + "_overlap.tif"))
 
         reset_roi_zones(camera)
+
+        # deactivate triggering
+        camera.TriggerMode.SetValue("On")
         
         camera.Close()
         stage.close()
+
+        return layer_adjustments
 
 def scan(stage: object, mid_point: tuple, scan_range: float, num_pix: int):
     # first move to mid-point y-value
@@ -178,15 +190,14 @@ def scan(stage: object, mid_point: tuple, scan_range: float, num_pix: int):
     stage.wait_for_device()
 
     vel = PIX_SIZE_MM * FPS_MAX
-    #vel = 0.25 #0.06
+    vel = 0.1
     print(f"Velocity: {vel}mm/sec")
 
     stage.set_speed(x=vel, y=vel)
 
     start = mid_point[0] - scan_range/2 - FOV_HEIGHT_MM/2
-    # stop = mid_point[0] + scan_range/2 + FOV_HEIGHT_MM/2
 
-    stage.scan_x_axis_enc(start=start, num_pix=num_pix)
+    stage.scan_x_axis_enc(start=start, num_pix=num_pix, enc_divide=38)
 
 
 if __name__ == "__main__":
@@ -197,30 +208,27 @@ if __name__ == "__main__":
 
     camera = camera_setup()
 
-    num_zones = 3
+    num_zones = 5 # number of z-slices
     mid_point = (0, 0)
     scan_range_factor = 1 # number of overlapping fov images
 
     scan_range = scan_range_factor * FOV_HEIGHT_MM
 
-    total_row_acq = SENSOR_HEIGHT_PIX * (scan_range_factor + 2) 
+    total_row_acq = SENSOR_HEIGHT_PIX * (scan_range_factor + 1) 
     
     set_roi_zones(camera, num_zones)
     scan(stage, mid_point=mid_point, scan_range=scan_range, num_pix=total_row_acq)
-    record_images(camera, stage, path, dirname, num_zones, total_row_acq)
+    layer_adjustments = record_images(camera, stage, path, dirname, num_zones, total_row_acq)
 
-    # description = input("Add description: ")
-    description = None
+    params = {
+        "total_reconstructions": num_zones,
+        "total_overlap_fovs": scan_range_factor,
+        "scan_midpoint": mid_point,
+        "scan_range": scan_range,
+        "layer_adjustments": layer_adjustments
+    }
 
-    fname = os.path.join(path, dirname + "_description.txt")
+    fname = os.path.join(path, dirname + "_params.json")
 
-    with open(fname, "w") as f:
-        f.write(f"Total image reconstructions: {num_zones}\n")
-        f.write(f"Total overlapping FOVs: {scan_range_factor}\n")
-        f.write(f"Scan midpoint: {mid_point}\n")
-        f.write(f"Scan range: {scan_range}\n")
-
-        if description:
-            f.write(f"Description: {description}")
-    
-    print("FINISHED")
+    with open(fname, "w") as file:
+        file.write(json.dumps(params))
