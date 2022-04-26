0dae0243be041c0e3a8dd905240327997fd3864e
lukasvasadi <lukas.vasadi@gmail.com>
2022-04-26 17:25:40 -0400

    separate camera code with new object

---
+++ scripts/basler/baslerace.py
@@ -0,0 +1,167 @@
+import os
+import math
+import numpy as np
+import tifffile as tf
+
+from pypylon import pylon, genicam
+from pypylon.pylon import InstantCamera, TlFactory
+
+
+class ACA2040(InstantCamera):
+    """Camera model"""
+
+    def __init__(
+        self,
+        pix_format: str = "Mono12p",
+        exp_time: int = 100,
+        frame_width: int = 2064,
+        num_buffers: int = 15,
+    ):
+        try:
+            super().__init__(TlFactory.GetInstance().CreateFirstDevice())
+
+            # open camera port
+            self.Open()
+
+            # allocate buffers for acquisitions
+            self.MaxNumBuffer.SetValue(num_buffers)
+
+            self.AcquisitionMode.SetValue("Continuous")
+
+            # Mono12p adds padding zeros to data bytes
+            self.PixelFormat.SetValue(pix_format)
+
+            # exposure time in micro-seconds
+            self.ExposureTime.SetValue(exp_time)
+
+            # offset must be zero if max width
+            self.Width.SetValue(frame_width)
+            self.OffsetX.SetValue(0)
+
+            # configure IO trigger
+            self.TriggerSelector.SetValue("FrameStart")
+            self.TriggerActivation.SetValue("FallingEdge")
+            self.TriggerSource.SetValue("Line1")
+            self.TriggerMode.SetValue("On")
+
+        except genicam.GenericException as e:
+            if "Device is exlusively opened by another client" in e:
+                print("Camera is opened in another program!")
+            else:
+                print("Exception: ", e)
+
+    @staticmethod
+    def save_image_array(img_array: object, filename: str):
+        # add an empty color channel
+        img_array = np.expand_dims(img_array, axis=0)
+
+        # rearrange data for multidimensional tiff standard
+        img_array = np.transpose(img_array, axes=[1, 0, 2, 3])
+
+        tf.imwrite(filename, img_array, imagej=True)
+
+    def reset_roi_zones(self):
+        if genicam.IsWritable(self.ROIZoneMode):
+            for i in range(8):
+                self.ROIZoneSelector.SetValue(f"Zone{i}")
+                self.ROIZoneMode.SetValue("Off")
+
+        if genicam.IsWritable(self.OffsetY):
+            # reset camera to full FOV
+            self.OffsetY.SetValue(0)
+            self.Height.SetValue(self.Height.Max)
+
+    def set_roi_zones(self, num_zones: int = 3):
+        # camera remembers previous settings
+        self.reset_roi_zones()
+
+        zone_spacing = math.trunc((self.Height.Max - 4) / (num_zones - 1))
+        zone_offset = 0
+
+        for i in range(num_zones):
+            self.ROIZoneSelector.SetValue(f"Zone{i}")
+            try:
+                self.ROIZoneOffset.SetValue(zone_offset)
+            except genicam.OutOfRangeException:
+                # camera offset must be divisible by minimum zone pix height
+                adjusted_offset = zone_offset
+                while adjusted_offset % 4:
+                    adjusted_offset -= 1
+                self.ROIZoneOffset.SetValue(adjusted_offset)
+            self.ROIZoneSize.SetValue(4)
+            self.ROIZoneMode.SetValue("On")
+
+            zone_offset += zone_spacing
+
+    def acquire(
+        self,
+        path: str,
+        dirname: str,
+        num_zones: int = 3,
+        total_row_acq: int = 1544,
+        frame_width: int = 2064,
+    ):
+        # initialize imaging data array
+        reconstruction = np.empty((num_zones, total_row_acq, frame_width), np.uint16)
+
+        # initialize counter to track acquisitions
+        acq_counter = 0
+
+        try:
+            self.StartGrabbingMax(total_row_acq, pylon.GrabStrategy_OneByOne)
+
+            while self.IsGrabbing():
+                # use timeout of 2000ms (must be greater than exposure time)
+                grab_result = self.RetrieveResult(
+                    5000, pylon.TimeoutHandling_ThrowException
+                )
+
+                if grab_result.GrabSucceeded():
+                    for i in range(num_zones):
+                        reconstruction[i][acq_counter] = grab_result.Array[
+                            i * 4
+                        ].reshape((1, 2064))
+                    acq_counter += 1
+                else:
+                    print(
+                        "Error: ", grab_result.ErrorCode, grab_result.ErrorDescription
+                    )
+                grab_result.Release()
+        except genicam.GenericException as e:
+            print(e)
+        finally:
+            self.StopGrabbing()
+
+            print(f"total images requested: {total_row_acq}")
+            print(f"total images recorded: {acq_counter}")
+
+            overlap = []
+
+            for i in range(num_zones):
+                self.ROIZoneSelector.SetValue(f"Zone{i}")
+                offset = self.ROIZoneOffset.GetValue()
+
+                start = self.Height.Max - offset
+                stop = total_row_acq - (self.Height.Max - start)
+
+                overlap.append(reconstruction[i, start:stop, :])
+
+            self.save_image_array(
+                np.stack(reconstruction, axis=0),
+                os.path.join(path, dirname + "_raw.tif"),
+            )
+
+            self.save_image_array(
+                np.stack(overlap, axis=0),
+                os.path.join(path, dirname + "_overlap.tif"),
+            )
+
+            self.close()
+
+    def close(self):
+        self.reset_roi_zones()
+
+        # deactivate triggering
+        self.TriggerMode.SetValue("Off")
+
+        self.Close()
--- scripts/scan_continuous_encoder_ttl.py
+++ scripts/scan_continuous_encoder_ttl.py
@@ -3,26 +3,17 @@
 # ===============================================================================
 
 import os
-import math
 import json
 import numpy as np
-import tifffile as tf
 import matplotlib.pyplot as plt
 
-from pypylon import pylon, genicam
 from datetime import datetime
 from asi.asistage import MS2000
+from basler.baslerace import ACA2040
 
 import nidaqmx
 from nidaqmx.stream_readers import CounterReader
-from nidaqmx.constants import (
-    AcquisitionType,
-    CountDirection,
-    Edge,
-    READ_ALL_AVAILABLE,
-    TaskMode,
-    TriggerType,
-)
+from nidaqmx.constants import AcquisitionType, CountDirection, Edge
 
 
 # explicit constants
@@ -34,6 +25,8 @@ FPS_MAX = 635
 
 # inferred constants
 FOV_HEIGHT_MM = SENSOR_HEIGHT_PIX * PIX_SIZE_MM
+SCAN_VEL_MM_S = PIX_SIZE_MM * FPS_MAX
+ENC_DIVIDE = PIX_SIZE_MM * 10e4
 
 
 def create_data_dir():
@@ -44,383 +37,105 @@ def create_data_dir():
     return (path, dirname)
 
 
-def reset_roi_zones(camera: object):
-    if genicam.IsWritable(camera.ROIZoneMode):
-        for i in range(8):
-            camera.ROIZoneSelector.SetValue(f"Zone{i}")
-            camera.ROIZoneMode.SetValue("Off")
-
-    if genicam.IsWritable(camera.OffsetY):
-        # reset camera to full FOV
-        camera.OffsetY.SetValue(0)
-        camera.Height = camera.Height.Max
-
-
-def set_roi_zones(camera: object, num_zones: int = 3):
-    reset_roi_zones(camera)
-
-    zone_spacing = math.trunc((SENSOR_HEIGHT_PIX - 4) / (num_zones - 1))
-    zone_offset = 0
-
-    for i in range(num_zones):
-        camera.ROIZoneSelector.SetValue(f"Zone{i}")
-        try:
-            camera.ROIZoneOffset.SetValue(zone_offset)
-        except genicam.OutOfRangeException:
-            # camera offset must be divisible by minimum zone pix height
-            adjusted_offset = zone_offset
-            while adjusted_offset % 4:
-                adjusted_offset -= 1
-            camera.ROIZoneOffset.SetValue(adjusted_offset)
-        camera.ROIZoneSize.SetValue(4)
-        camera.ROIZoneMode.SetValue("On")
-
-        zone_offset += zone_spacing
-
-
-def camera_setup():
-    try:
-        # grab camera
-        camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
-        camera.Open()
-
-        camera.AcquisitionMode.SetValue("Continuous")
-
-        # Mono12p adds padding zeros to data bytes
-        camera.PixelFormat = "Mono12p"
-
-        # exposure time range: 20.0 to 10000000.0 us
-        camera.ExposureTime = EXPOSURE_TIME_US
-
-        # sensor pixel dimensions: 2064 x 1544
-        camera.Width = camera.Width.Max
-        camera.OffsetX.SetValue(0)
-
-        # number of buffers allocated for acquisitions
-        camera.MaxNumBuffer = 15
-
-        # configure IO trigger
-        camera.TriggerSelector.SetValue("FrameStart")
-        camera.TriggerActivation.SetValue("FallingEdge")
-        camera.TriggerSource.SetValue("Line1")
-        camera.TriggerMode.SetValue("On")
-
-        return camera
-    except genicam.GenericException as e:
-        if "Device is exlusively opened by another client" in e:
-            print("Camera is opened in another program!")
-        else:
-            print("Exception: ", e)
-        return None
-
-
-def save_image_array(img_array: object, filename: str):
-    # add an empty color channel
-    img_array = np.expand_dims(img_array, axis=0)
-
-    # rearrange data for multidimensional tiff standard
-    img_array = np.transpose(img_array, axes=[1, 0, 2, 3])
-
-    tf.imwrite(filename, img_array, imagej=True)
-
-
-def record_images(
-    camera: object,
-    stage: object,
-    path: str,
-    dirname: str,
-    num_zones: int = 3,
-    total_row_acq: int = 1544,
-):
-    # initialize data array
-    reconstruction = np.empty((num_zones, total_row_acq, SENSOR_WIDTH_PIX), np.uint16)
-
-    total_ticks = total_row_acq * 2
-
-    try:
-        ci_task = nidaqmx.Task()
-        channel = ci_task.ci_channels.add_ci_count_edges_chan(
-            "Dev1/ctr0",
-            initial_count=0,
-            edge=Edge.FALLING,
-            count_direction=CountDirection.COUNT_UP,
-        )
-        channel.ci_count_edges_term = "PFI0"  # "PFI0"
-
-        ci_task.timing.cfg_samp_clk_timing(
-            rate=500000,
-            source="PFI1",
-            active_edge=Edge.RISING,
-            sample_mode=AcquisitionType.FINITE,
-            samps_per_chan=total_ticks,
-        )
-
-        ci_task.in_stream.input_buf_size = total_ticks  # should there be more?
-        reader = CounterReader(ci_task.in_stream)
-        reader.read_all_avail_samp = True
-        ci_task.start()
-
-        # use the first-in-first-out processing approach
-        camera.StartGrabbingMax(total_row_acq, pylon.GrabStrategy_OneByOne)
-
-        counter = 0
-
-        while camera.IsGrabbing():
-            # use timeout of 2000ms (must be greater than exposure time)
-            grab_result = camera.RetrieveResult(
-                5000, pylon.TimeoutHandling_ThrowException
-            )
-            # try to get actual position of the acquired image
-
-            if grab_result.GrabSucceeded():
-                for i in range(num_zones):
-                    reconstruction[i][counter] = grab_result.Array[i * 4].reshape(
-                        (1, 2064)
-                    )
-                counter += 1
-            else:
-                print("Error: ", grab_result.ErrorCode, grab_result.ErrorDescription)
-            grab_result.Release()
-    except genicam.GenericException as e:
-        print(e)
-    finally:
-        print(f"total images requested: {total_row_acq}")
-        print(f"total images recorded: {counter}")
-        camera.StopGrabbing()
-
-        count_array = np.zeros(total_row_acq, dtype=np.uint32)
-        reader.read_many_sample_uint32(
-            count_array, number_of_samples_per_channel=total_row_acq, timeout=0.1
-        )
-        plt.plot(count_array)
-        plt.grid()
-        plt.show()
-        # counts = np.array(ci_task.read(number_of_samples_per_channel=nidaqmx.constants.READ_ALL_AVAILABLE,timeout=1.0))
-        # deltas = (counts[1:]-counts[:-1])
-        deltas = count_array[1:] - count_array[:-1]
-        plt.plot([0, total_row_acq], [0, total_row_acq], "r--")
-        plt.plot(deltas, "k", alpha=0.5)
-        plt.plot(count_array)
-        plt.grid()
-        plt.title("Skipped frames")
-        plt.xlabel("Encoder ticks")
-        plt.ylabel("Images captured")
-        plt.show()
-        # print(f'pulse widths (msec): {deltas}')
-        ci_task.stop()
-        ci_task.close()
-
-        layer_adjustments = {}
-
-        overlap = []
-
-        for i in range(num_zones):
-            camera.ROIZoneSelector.SetValue(f"Zone{i}")
-            offset = camera.ROIZoneOffset.GetValue()
-
-            start = SENSOR_HEIGHT_PIX - offset
-            stop = total_row_acq - (SENSOR_HEIGHT_PIX - start)
-
-            overlap.append(reconstruction[i, start:stop, :])
-
-            layer_adjustments[f"{i}"] = {"offset": offset, "start": start, "stop": stop}
-
-        save_image_array(
-            np.stack(reconstruction, axis=0), os.path.join(path, dirname + "_raw.tif")
-        )
-        save_image_array(
-            np.stack(overlap, axis=0), os.path.join(path, dirname + "_overlap.tif")
-        )
-
-        reset_roi_zones(camera)
-
-        # deactivate triggering
-        camera.TriggerMode.SetValue("Off")
-
-        camera.Close()
-        stage.close()
-
-        return layer_adjustments
-
-
-def scan_record_images(
-    mid_point: tuple,
-    scan_range: float,
-    num_pix: int,
-    camera: object,
-    stage: object,
-    path: str,
-    dirname: str,
-    num_zones: int = 3,
-    total_row_acq: int = 1544,
-):
-
-    # initialize data array
-    reconstruction = np.empty((num_zones, total_row_acq, SENSOR_WIDTH_PIX), np.uint16)
-
-    total_ticks = total_row_acq  # * 2
-
-    try:
-        ci_task = nidaqmx.Task()
-        channel = ci_task.ci_channels.add_ci_count_edges_chan(
-            "Dev1/ctr0",
-            initial_count=0,
-            edge=Edge.FALLING,
-            count_direction=CountDirection.COUNT_UP,
-        )
-        channel.ci_count_edges_term = "PFI0"  # "PFI0"
-
-        ci_task.timing.cfg_samp_clk_timing(
-            rate=50000,
-            source="PFI1",
-            active_edge=Edge.RISING,
-            sample_mode=AcquisitionType.FINITE,
-            samps_per_chan=total_ticks,
-        )
-
-        ci_task.in_stream.input_buf_size = total_ticks  # should there be more?
-        reader = CounterReader(ci_task.in_stream)
-        reader.read_all_avail_samp = True
-        ci_task.start()
-
-        # use the first-in-first-out processing approach
-        camera.StartGrabbingMax(total_row_acq, pylon.GrabStrategy_OneByOne)
-
-        scan(stage, mid_point=mid_point, scan_range=scan_range, num_pix=total_row_acq)
-
-        counter = 0
-
-        while camera.IsGrabbing():
-            # use timeout of 2000ms (must be greater than exposure time)
-            grab_result = camera.RetrieveResult(
-                5000, pylon.TimeoutHandling_ThrowException
-            )
-            # try to get actual position of the acquired image
-
-            if grab_result.GrabSucceeded():
-                for i in range(num_zones):
-                    reconstruction[i][counter] = grab_result.Array[i * 4].reshape(
-                        (1, 2064)
-                    )
-                counter += 1
-            else:
-                print("Error: ", grab_result.ErrorCode, grab_result.ErrorDescription)
-            grab_result.Release()
-    except genicam.GenericException as e:
-        print(e)
-    finally:
-        print(f"total images requested: {total_row_acq}")
-        print(f"total images recorded: {counter}")
-        camera.StopGrabbing()
-
-        count_array = np.zeros(total_row_acq, dtype=np.uint32)
-        reader.read_many_sample_uint32(
-            count_array, number_of_samples_per_channel=total_row_acq, timeout=0.1
-        )
-        plt.plot(count_array)
-        plt.grid()
-        plt.show()
-        # counts = np.array(ci_task.read(number_of_samples_per_channel=nidaqmx.constants.READ_ALL_AVAILABLE,timeout=1.0))
-        # deltas = (counts[1:]-counts[:-1])
-        deltas = count_array[1:] - count_array[:-1]
-        plt.plot([0, total_row_acq], [0, total_row_acq], "r--")
-        plt.plot(deltas, "k", alpha=0.5)
-        plt.plot(count_array)
-        plt.grid()
-        plt.title("Skipped frames")
-        plt.xlabel("Encoder ticks")
-        plt.ylabel("Images captured")
-        plt.show()
-        # print(f'pulse widths (msec): {deltas}')
-        ci_task.stop()
-        ci_task.close()
-
-        layer_adjustments = {}
-
-        overlap = []
-
-        for i in range(num_zones):
-            camera.ROIZoneSelector.SetValue(f"Zone{i}")
-            offset = camera.ROIZoneOffset.GetValue()
-
-            start = SENSOR_HEIGHT_PIX - offset
-            stop = total_row_acq - (SENSOR_HEIGHT_PIX - start)
-
-            overlap.append(reconstruction[i, start:stop, :])
-
-            layer_adjustments[f"{i}"] = {"offset": offset, "start": start, "stop": stop}
-
-        save_image_array(
-            np.stack(reconstruction, axis=0), os.path.join(path, dirname + "_raw.tif")
-        )
-        save_image_array(
-            np.stack(overlap, axis=0), os.path.join(path, dirname + "_overlap.tif")
-        )
-
-        reset_roi_zones(camera)
-
-        # deactivate triggering
-        camera.TriggerMode.SetValue("Off")
-
-        camera.Close()
-        stage.close()
-
-        return layer_adjustments
-
-
 def scan(stage: object, mid_point: tuple, scan_range: float, num_pix: int):
     # first move to mid-point y-value
     stage.set_speed(x=1, y=1)
     stage.move_axis("Y", mid_point[1])
     stage.wait_for_device()
 
-    vel = PIX_SIZE_MM * FPS_MAX
-    vel = 0.5
-    # vel = 0.75
-    print(f"Velocity: {vel}mm/sec")
-
-    stage.set_speed(x=vel, y=vel)
+    stage.set_speed(x=SCAN_VEL_MM_S, y=SCAN_VEL_MM_S)
 
     start = mid_point[0] - scan_range / 2 - FOV_HEIGHT_MM / 2
 
-    stage.scan_x_axis_enc(start=start, num_pix=num_pix, enc_divide=38)
+    stage.scan_x_axis_enc(start=start, num_pix=num_pix, enc_divide=ENC_DIVIDE)
+
+    stage.close()
+
+
+def daq_config(total_row_acq: int) -> tuple:
+    total_ticks = total_row_acq
+
+    ci_task = nidaqmx.Task()
+
+    channel = ci_task.ci_channels.add_ci_count_edges_chan(
+        "Dev1/ctr0",
+        initial_count=0,
+        edge=Edge.FALLING,
+        count_direction=CountDirection.COUNT_UP,
+    )
+
+    channel.ci_count_edges_term = "PFI0"
+
+    ci_task.timing.cfg_samp_clk_timing(
+        rate=50000,
+        source="PFI1",
+        active_edge=Edge.RISING,
+        sample_mode=AcquisitionType.FINITE,
+        samps_per_chan=total_ticks,
+    )
+
+    ci_task.in_stream.input_buf_size = total_ticks
+
+    reader = CounterReader(ci_task.in_stream)
+    reader.read_all_avail_samp = True
+
+    ci_task.start()
+
+    return (ci_task, reader)
+
+
+def show_daq_data(ci_task: object, reader: object):
+    count_array = np.zeros(total_row_acq, dtype=np.uint32)
+    reader.read_many_sample_uint32(
+        count_array, number_of_samples_per_channel=total_row_acq, timeout=0.1
+    )
+    plt.plot(count_array)
+    plt.grid()
+    plt.show()
+    # counts = np.array(ci_task.read(number_of_samples_per_channel=nidaqmx.constants.READ_ALL_AVAILABLE,timeout=1.0))
+    # deltas = (counts[1:]-counts[:-1])
+    deltas = count_array[1:] - count_array[:-1]
+    plt.plot([0, total_row_acq], [0, total_row_acq], "r--")
+    plt.plot(deltas, "k", alpha=0.5)
+    plt.plot(count_array)
+    plt.grid()
+    plt.title("Skipped frames")
+    plt.xlabel("Encoder ticks")
+    plt.ylabel("Images captured")
+    plt.show()
+    # print(f'pulse widths (msec): {deltas}')
+    ci_task.stop()
+    ci_task.close()
 
 
 if __name__ == "__main__":
     path, dirname = create_data_dir()
 
-    # create MS-2000 serial interface
-    stage = MS2000("COM3", 115200)
-
-    camera = camera_setup()
-
-    num_zones = 5  # number of z-slices
-    mid_point = (0, 0)
-    scan_range_factor = 1  # number of overlapping fov images
-
+    mid_point = (0, 0)  # scan mid-point
+    num_zones = 5  # number of image slices
+    scan_range_factor = 1  # number of overlapping fovs
     scan_range = scan_range_factor * FOV_HEIGHT_MM
-
     total_row_acq = SENSOR_HEIGHT_PIX * (scan_range_factor + 1)
 
-    set_roi_zones(camera, num_zones)
-    layer_adjustments = scan_record_images(
-        mid_point,
-        scan_range,
-        total_row_acq,
-        camera,
-        stage,
-        path,
-        dirname,
-        num_zones,
-        total_row_acq,
-    )
+    # connect to stage serial port
+    stage = MS2000("COM3", 115200)
+
+    # configure camera object
+    camera = ACA2040(exp_time=EXPOSURE_TIME_US)
+    camera.set_roi_zones(num_zones)
+
+    ci_task, reader = daq_config(total_row_acq)
+
+    # initiate scan and data acquisition
+    scan(stage, mid_point=mid_point, scan_range=scan_range, num_pix=total_row_acq)
+    camera.acquire(path, dirname, num_zones, total_row_acq)
+    show_daq_data(ci_task, reader)
 
     params = {
         "total_reconstructions": num_zones,
         "total_overlap_fovs": scan_range_factor,
         "scan_midpoint": mid_point,
         "scan_range": scan_range,
-        "layer_adjustments": layer_adjustments,
     }
 
     fname = os.path.join(path, dirname + "_params.json")
