a492654f5a2c25990b45409fa93c4432daee8614
lukasvasadi <lukas.vasadi@gmail.com>
2022-05-03 13:30:59 -0400

    add led array serial device class

--- scripts/adafruit/matrixportal.py
+++ scripts/adafruit/matrixportal.py
@@ -1,10 +1,13 @@
 import serial
 
 from serial import SerialException
+from serial.tools import list_ports
 
 
-class MatrixPortal:
-    def __init__(self, port: str = "COM3", baudrate: int = 115200):
+class SerialPort:
+    """Managing RS232 serial connection"""
+
+    def __init__(self, port: str, baudrate: int):
         # configure serial connection
         self.serial_port = serial.Serial()
         self.serial_port.port = port
@@ -16,6 +19,9 @@ class MatrixPortal:
         self.serial_port.dsrdtr = False
         self.serial_port.timeout = 1
 
+        # set size of rx/tx buffers before opening serial port
+        self.serial_port.set_buffer_size(rx_size=12800, tx_size=12800)
+
         # open serial port
         try:
             self.serial_port.open()
@@ -26,9 +32,40 @@ class MatrixPortal:
             # clear buffers
             self.serial_port.reset_input_buffer()
             self.serial_port.reset_output_buffer()
-            print("MatrixPortal connection successful")
+            print("MS-2000 connection successful")
 
-    def read(self) -> str:
-        response = self.serial_port.readline().decode("utf-8", "ignore").strip()
+    @staticmethod
+    def find_port(descriptor):
+        ports = list(list_ports.comports())
+        for com in ports:
+            if descriptor in com.description:
+                return com[0]
 
-        return response
+    def send_command(self, cmd: str):
+        # good practice to reset buffers before each transmission
+        self.serial_port.reset_input_buffer()
+        self.serial_port.reset_output_buffer()
+
+        cmd = cmd + "\r"
+        self.serial_port.write(cmd.encode())  # pass as bytearray
+
+    def read_response(self) -> str:
+        return self.serial_port.readline().decode("utf-8", "ignore").strip()
+
+    def close(self):
+        if self.serial_port.is_open:
+            self.serial_port.close()
+
+
+class MatrixPortal(SerialPort):
+    # valid baudrates
+    BAUDRATES = [9600, 19200, 28800, 115200]
+
+    def __init__(self, port: str, baudrate: int = 115200):
+        super().__init__(port, baudrate)
+
+        # validate user baudrate
+        if baudrate not in self.BAUDRATES:
+            raise ValueError(
+                "Invalid baudrate. Valid rates include 9600, 19200, 28800, 115200"
+            )
