8fc3abec1bb82a4dca39273837d86d60a7bf2db5
lukasvasadi <lukas.vasadi@gmail.com>
2022-05-04 13:06:45 -0400

    restrict velocity decimal positions for MS2000

--- scripts/calibration.py
+++ scripts/calibration.py
@@ -41,7 +41,7 @@ def calc_col_filter(img_arr: ndarray) -> ndarray:
     col_filter_vals = np.empty((img_arr.shape[0], img_arr.shape[2]), dtype=np.float64)
 
     for i in range(img_arr.shape[0]):
-        col_filter_vals[i] = np.mean(img_arr[i], axis=0)
+        col_filter_vals[i] = np.average(img_arr[i], axis=0)
 
     return col_filter_vals
 
@@ -66,6 +66,8 @@ if __name__ == "__main__":
     cam.set_trigger()
     cam.set_io_control(line=2, source="ExposureActive")
 
+    stage_vel = "{:.4f}".format(cam.sensor_pix_size_mm * cam.fps_max / 2)
+
     confirm = input(
         """This process will overwrite the current filter data. """
         """Make sure that the optical path is free of samples. """
@@ -79,7 +81,15 @@ if __name__ == "__main__":
             cam.set_roi_zones(z)
 
             # initiate scan and data acquisition
-            scan(stage, mid_point, scan_range, total_row_acq)
+            scan(
+                stage,
+                stage_vel,
+                mid_point,
+                scan_range,
+                cam.fov_height_mm,
+                cam.sensor_pix_size_mm * 1e5,
+                total_row_acq,
+            )
             img = cam.acquire_stack(z, total_row_acq)
 
             col_filter_vals = calc_col_filter(img)
@@ -90,7 +100,7 @@ if __name__ == "__main__":
             vals[str(z)] = col_filter_vals.tolist()
 
         # save filtering values
-        with open("./scripts/processing/filter_vals.json", "w") as file:
+        with open("./scripts/processing/cal_data_tilt.json", "w") as file:
             file.write(json.dumps(vals, indent=4))
 
     # housekeeping
