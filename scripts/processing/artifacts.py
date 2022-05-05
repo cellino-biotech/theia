64113c6c1a59abba24721d0e82869cd2e9f47bf1
lukasvasadi <lukas.vasadi@gmail.com>
2022-05-05 12:01:51 -0400

    update artifact data

--- scripts/processing/artifacts.py
+++ scripts/processing/artifacts.py
@@ -6,28 +6,28 @@ import numpy as np
 from numpy import ndarray
 
 
-def apply_col_filter(img_arr: ndarray):
+def filter_col_artifacts(arr: ndarray) -> ndarray:
     """Remove artifact interference patterns from imaging planes
 
     Assume filter values file is located in same parent directory
     """
     file_dir = os.path.join(
-        os.path.dirname(os.path.realpath(__file__)), "cal_data_tilt.json"
+        os.path.dirname(os.path.realpath(__file__)), "artifact_data_tilt.json"
     )
     if os.path.exists(file_dir):
         with open(file_dir) as file:
             cal_data = json.loads(file.read())
 
         # extract data corresponding to the number of image planes
-        cal_data = cal_data[str(img_arr.shape[0])]
+        cal_data = cal_data[str(arr.shape[0])]
 
         # use either amin or mean
-        cal_data = np.multiply(cal_data, (1 / np.mean(cal_data)))
+        cal_data = np.multiply(cal_data, (1 / np.amin(cal_data)))
 
-        for p in range(img_arr.shape[0]):
-            for r in range(img_arr.shape[1]):
-                img_arr[p][r] = img_arr[p][r] / cal_data[p]
+        for p in range(arr.shape[0]):
+            for r in range(arr.shape[1]):
+                arr[p][r] = arr[p][r] / cal_data[p]
 
-        return img_arr
+        return arr
     else:
         warnings.warn("Missing filter values file!")
