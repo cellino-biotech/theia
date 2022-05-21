0170ef394250bb9f65a2655e19a4425d3c682bf4
lukasvasadi <lukas.vasadi@gmail.com>
2022-05-21 14:27:45 -0400

    add example script for basic daq control

---
+++ scripts/ni/lamp.py
@@ -0,0 +1,11 @@
+import nidaqmx
+
+
+def illuminator(on_state: bool = True):
+    with nidaqmx.Task() as task:
+        task.do_channels.add_do_chan("Dev1/port0/line0")
+        task.write(on_state)
+
+
+if __name__ == "__main__":
+    illuminator(True)
